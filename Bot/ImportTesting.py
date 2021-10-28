e0afeaeb8e84eb9e121eafe438ea7a33d65ccc01
Nick1214 <74779897+Nick1214@users.noreply.github.com>
2021-10-28 13:39:03 +0300

    Update ImportTesting.py

--- Bot/ImportTesting.py
+++ Bot/ImportTesting.py
@@ -1,6 +1,6 @@
 
 try:
     import MainDirectory.Parser_For_Open_Profile
-    print(MainDirectory.Parser_For_Open_Profile.get_profile_information(username="neochiter22"))
+    print(MainDirectory.Parser_For_Open_Profile.get_profile_information(username="any instagram username"))
 except ImportError:
     print("Can't Import this module")
